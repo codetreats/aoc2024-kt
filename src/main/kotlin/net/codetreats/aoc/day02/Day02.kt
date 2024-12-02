c32bcb80a9dfe890da15b7f1f851b2e11b9ff1f2
Martin <github@msturm.net>
2024-12-02 10:07:12 +0100

    add day02

--- src/main/kotlin/net/codetreats/aoc/day02/Day02.kt
+++ src/main/kotlin/net/codetreats/aoc/day02/Day02.kt
@@ -2,19 +2,32 @@ package net.codetreats.aoc.day02
 
 import net.codetreats.aoc.Day
 import net.codetreats.aoc.util.Logger
+import kotlin.math.sign
 
-class Day02 : Day<List<String>>(2) {
+class Day02 : Day<List<List<Int>>>(2) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override val useDummy = true
+    override val useDummy = false
 
-    override fun convert(input: List<String>): List<String> = input
+    override fun convert(input: List<String>): List<List<Int>> =
+        input.map { line -> line.split("\\s+".toRegex()).map { it.toInt() } }
 
-    override fun run1(data: List<String>): String {
-        return ""
+    override fun run1(data: List<List<Int>>): String = data.count { report -> report.isSafe() }.toString()
+
+    override fun run2(data: List<List<Int>>): String = data.count { report -> report.generateSubsets().any { it.isSafe() } }.toString()
+
+    private fun List<Int>.isSafe(): Boolean {
+        val sign = sign(get(0).toDouble() - get(1).toDouble()).toInt()
+        for (i in 1 until size) {
+            val diff = (get(i - 1) - get(i)) * sign
+            if (diff !in 1..3) {
+                return false
+            }
+        }
+        return true
     }
 
-    override fun run2(data: List<String>): String {
-        return ""
+    private fun List<Int>.generateSubsets(): List<List<Int>> = indices.map { index ->
+        filterIndexed { i, _ -> i != index }
     }
 }
--- src/test/kotlin/net/codetreats/aoc/day02/Day02Test.kt
+++ src/test/kotlin/net/codetreats/aoc/day02/Day02Test.kt
@@ -17,21 +17,21 @@ class Day02Test {
 
     @Test
     fun testPart01() {
-        assertEquals("", classUnderTest.run1(false))
+        assertEquals("282", classUnderTest.run1(false))
     }
 
     @Test
     fun testPart01_dummyData() {
-        assertEquals("", classUnderTest.run1(true))
+        assertEquals("2", classUnderTest.run1(true))
     }
 
     @Test
     fun testPart02() {
-        assertEquals("", classUnderTest.run2(false))
+        assertEquals("349", classUnderTest.run2(false))
     }
 
     @Test
     fun testPart02_dummyData() {
-        assertEquals("", classUnderTest.run2(true))
+        assertEquals("4", classUnderTest.run2(true))
     }
 }
