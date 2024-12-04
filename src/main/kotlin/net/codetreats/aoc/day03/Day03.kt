93a1e140f89cb9c9002736c2adc719325460bc9e
Martin <github@msturm.net>
2024-12-04 08:17:55 +0100

    add day03

--- src/main/kotlin/net/codetreats/aoc/day03/Day03.kt
+++ src/main/kotlin/net/codetreats/aoc/day03/Day03.kt
@@ -3,18 +3,40 @@ package net.codetreats.aoc.day03
 import net.codetreats.aoc.Day
 import net.codetreats.aoc.util.Logger
 
-class Day03 : Day<List<String>>(3) {
+class Day03 : Day<String>(3) {
+    private val mul = "^mul\\([1-9][0-9]?[0-9]?,[1-9][0-9]?[0-9]?\\).*\$".toRegex()
+    private val splitter = Regex("(?=(do\\(\\)|don't\\(\\)|mul))")
+
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override val useDummy = true
+    override val useDummy = false
 
-    override fun convert(input: List<String>): List<String> = input
+    override fun convert(input: List<String>): String = input.joinToString("")
 
-    override fun run1(data: List<String>): String {
-        return ""
+    override fun run1(data: String): String = splitter.split(data).sumOf { it.asMul() }.toString()
+
+    override fun run2(data: String): String {
+        var enabled = true
+        var sum = 0
+        splitter.split(data).forEach { part ->
+            if (part.startsWith("do()")) {
+                enabled = true
+            }
+            if (part.startsWith("don't()")) {
+                enabled = false
+            }
+            if (enabled) {
+                sum += part.asMul()
+            }
+        }
+        return sum.toString()
     }
 
-    override fun run2(data: List<String>): String {
-        return ""
+    private fun String.asMul() : Int {
+        if (!matches(mul)) {
+            return 0
+        }
+        val parts = substring(3).split("(",",",")")
+        return parts[1].toInt() * parts[2].toInt()
     }
 }
